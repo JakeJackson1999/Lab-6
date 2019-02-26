508be028c8798891b30e3f6d9310716536adaba8
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-02-26 11:44:14 -0600

    created Rectangle and finished it

---
+++ Rectangle.java
@@ -0,0 +1,21 @@
+
+public class Rectangle extends Polygon{
+	
+	private double height, width;
+	
+	public Rectangle(String id, double height, double width) {
+		super(id);
+		this.height = height;
+		this.width = width;
+	}
+	
+	public double getArea() {
+		return height * width;
+	}
+	
+	@Override
+	public String getShapeType() {
+		return "Rectangle";
+	}
+
+}
